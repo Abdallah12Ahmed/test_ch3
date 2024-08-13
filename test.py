ea0303be05ad9f8f0d64f1130ce0894ed486d6b2
karim <kareemfrance02@gamil.com>
2024-08-13 13:22:51 +0300

    The Project uploaded

---
+++ test.py
@@ -0,0 +1,14 @@
+def func_average(numb1,numb2,numb3):
+	 avg=(numb1+numb2+numb3)/3
+ 	 return avg
+
+numb1 = int(input("Enter the 1st  number"))
+numb2 = int(input("Enter the 2nd number"))
+numb3 = int(input("Enter the 3rd number"))
+
+print(func_average(numb1,numb2,numb3))
+
+
+
+
+
